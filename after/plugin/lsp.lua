1ab1355450f9570026ee2d541c0488810dcb42ce
Arthur Kuz <arthur.kuz@protonmail.com>
2024-06-20 13:53:14 +0300

    Add typo lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -91,6 +91,19 @@ require('lspconfig').eslint.setup({
   end,
 })
 
+require('lspconfig').typos_lsp.setup({
+    -- Logging level of the language server. Logs appear in :LspLog. Defaults to error.
+    cmd_env = { RUST_LOG = "error" },
+    init_options = {
+        -- Custom config. Used together with any workspace config files, taking precedence for
+        -- settings declared in both. Equivalent to the typos `--config` cli argument.
+        config = '~/code/typos-lsp/crates/typos-lsp/tests/typos.toml',
+        -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
+        -- Defaults to error.
+        diagnosticSeverity = "Warning"
+    }
+})
+
 -- Show diagnostic info inline
 vim.diagnostic.config({
   virtual_text = true,
